1e47e902728854f1e03732b9b88dd6aef3a550b1
juanjinario <juanjco2@hotmail.com>
2020-11-15 19:36:15 +0100

    Actualizar version de Angular cli

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{ path: 'users', loadChildren: () => import('./pages/users/users.module').then(m => m.UsersModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
